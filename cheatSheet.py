c168f23aa9d04456ae39e1fbcfd65ad17dae904c
Alex Servirog <servalex1@gmail.com>
2015-12-18 00:58:32 +0200

    finished chapter 5.1

--- cheatSheet.py
+++ cheatSheet.py
@@ -337,7 +337,48 @@ for x in [1,2,3]:                       # equivalent
 vec = [-4, -2, 0, 2, 4]
 [x*2 for x in vec]                      # double
 [x for x in vec if x >=0]               # filter
-[abs(x) for x in vec]
-strs = [' asdf ',
+[abs(x) for x in vec]                   # abss
+
+strs = [' asdf ', '    s']
+[item.strip() for item in strs]
+
+[(x, x**2) for x in range(6)]           # tuples mast be parenthesized
+
+vec = [[1,2,3], [4,5,6], [7,8,9]]
+[num for elem in vec for num in elem]   # flatten the list
+
+from math import pi
+[str(round(pi, i)) for i in range(1, 6)] 
+
+
+# nested list comprehension
+
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
+]
+
+[[row[i] for row in matrix] for i in range(4)] # translates rows into columns
+
+transposed = []                         # this is equivalent
+for i in range(4):
+    transposed.append([row[i] for row in matrix])
+
+transposed = []                         # this is also equivalent
+for i in range(4):
+    transposed_row = []
+    for row in matrix:
+        transposed_row.append(row[i])
+    transposed.append(transposed_row)
+
+list(zip(*matrix))                      # there is a built in function zip for that and * also metters
+                                        # * unpacks argument lists
+
+
+# 5.2   del statement
+
+
+
 
 '----------------------------------------------------------------------------------------------------'
